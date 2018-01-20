b63056649c747c816d3d421586a15ca71295b850
Jonas Andersson Hultgren <jonas.andersson00@gmail.com>
2018-01-20 09:44:20 +0100

    Added player selection to Player Stats page

--- src/Elo.WebApp/ClientApp/components/PlayerStats/PlayerStatsPage.tsx
+++ src/Elo.WebApp/ClientApp/components/PlayerStats/PlayerStatsPage.tsx
@@ -3,35 +3,60 @@ import { RouteComponentProps, withRouter } from 'react-router';
 import { Head2HeadRecords } from './Head2HeadRecords';
 import { LatestGames } from '../Games/LatestGames';
 import { ExpectedScores } from './ExpectedScores';
+import { PlayerStatsLink } from '../Common/PlayerStatsLink';
+import * as Api from '../../api';
 
 interface PlayerStatsPageProps {
-    player: string;
+    player?: string;
 }
 
-export class PlayerStatsPage extends React.Component<RouteComponentProps<PlayerStatsPageProps>, {}> {
-    player: string;
+interface PlayerStatsPageState {
+    players: string[];
+}
+
+export class PlayerStatsPage extends React.Component<RouteComponentProps<PlayerStatsPageProps>, PlayerStatsPageState> {
+    player?: string;
 
     constructor(props: RouteComponentProps<PlayerStatsPageProps>) {
         super(props);
 
         this.player = this.props.match.params.player;
+        this.state = { players: [] };
+
+        Api.getPlayers().then(data => this.setState({ players: data }));
     }
 
     public render() {
         return <div>
-            <div className="page-header">
-                <h1>{this.player} <small>Player Stats</small></h1>
+            <div className="dropdown">
+                <button className="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">
+                    Select player <span className="caret"/>
+                </button>
+                <ul className="dropdown-menu">
+                    {this.state.players.map(player =>
+                        <li key={player}>
+                            <PlayerStatsLink player={player} />
+                        </li>
+                    )}
+                </ul>
             </div>
-            <div className="row">
-                <div className="col-sm-6">
-                    <Head2HeadRecords player={this.player} />
-                    <hr />
-                    <LatestGames player={this.player} numGames={10} headerSize={2} />
+            {this.player !== undefined &&
+                <div>
+                    <div className="page-header">
+                        <h1>{this.player} <small>Player Stats</small></h1>
+                    </div>
+                    <div className="row">
+                        <div className="col-sm-6">
+                            <Head2HeadRecords player={this.player} />
+                            <hr />
+                            <LatestGames player={this.player} numGames={10} headerSize={2} />
+                        </div>
+                        <div className="col-sm-6">
+                            <ExpectedScores player={this.player} />
+                        </div>
+                    </div>
                 </div>
-                <div className="col-sm-6">
-                    <ExpectedScores player={this.player} />
-                </div>
-            </div>
+            }
         </div>;
     }
 }
