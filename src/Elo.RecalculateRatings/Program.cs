acf31cd949f748c0594865e22df939451dbec1f3
Jonas Andersson Hultgren <jonas.andersson00@gmail.com>
2018-04-03 09:21:30 +0200

    Added log outputs

--- src/Elo.RecalculateRatings/Program.cs
+++ src/Elo.RecalculateRatings/Program.cs
@@ -10,13 +10,17 @@ namespace Elo.RecalculateRatings
         static void Main(string[] args)
         {
             // delete all ratings and stats
+            Console.WriteLine("Delete all ratings");
             RatingHandler.DeleteAllRatings();
+            Console.WriteLine("Delete all player stats");
             PlayerHandler.DeleteAllPlayerSeasons();
 
             // get all games in chronological order
+            Console.WriteLine("Get games");
             var games = GameHandler.GetGamesAfter(DateTime.MinValue, SortOrder.Ascending);
 
             // calculate new ratings
+            Console.WriteLine("Calculate new ratings");
             games.ForEach(g => Ratings.CalculateNewRatings(g));
         }
     }
